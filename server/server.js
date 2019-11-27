350d8c1c908a3d70c54124964d2ceae808074ff7
Mikiyas721 <Mikiyas721@gmail.com>
2019-11-27 10:01:15 -0600

    Added get in server.jst

--- server/server.js
+++ server/server.js
@@ -2,6 +2,15 @@
  * Rest API contains basic CRUD operation
  * Https POST method is used to create a resource
  **/
+
+/**
+ * Any of the express methods like post and get take two parameters:
+ * 1. Route
+ * 2. Call Back function called with the request and response
+ *  -- we can send a response using response.send()
+ *  -- we can get the request using request.body()
+ *  A best practice when send a response is to send it in an object as a field, this way we can send other things as well
+ **/
 let express = require('express');
 let bodyParser = require('body-parser');
 
@@ -22,6 +31,14 @@ app.post('/todos', (request, response) => {
         response.send(result);
     }, (error) => {
         response.status(400).send(error);
+    });
+});
+
+app.get('/todos', (request, response) => {
+    Todo.find().then((result) => {
+        response.send({result})
+    }, (error) => {
+        response.status(404).send(error);
     });
 });
 
---
+++ server/test/server.test.js
@@ -0,0 +1,1 @@
+//Todo add test code for each route
