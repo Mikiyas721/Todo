94ce9f6e4c275aa180715cbab4b19818b54bf4ba
Mikiyas721 <Mikiyas721@gmail.com>
2019-11-27 10:16:16 -0600

    Added Mongoose comment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,3 +1,14 @@
+/**
+ * Different Mongoose Queries:
+ * 1.find()
+ * 2.findOne()
+ * The above two methods take an object with the _id prop set
+ * 3.findById()
+ * Takes the id directly. In all the methods we don't need to pass an ObjectId like the Mongo native library.
+ * If Id isn't found the above methods don't fire an error rather return an empty array
+ * An error if fired if the id is invalid.The ObjectId in the mongodb library validates id
+ **/
+
 let mongoose = require('mongoose');
 
 mongoose.promise = global.promise;
